e8eaf5a2de4d35549a0018e0670e1801d089c8a5
Ramon Gebben <ramon@ra-ge.net>
2016-02-19 21:22:11 +0100

    merge

--- app/src/components/PullRequest.jsx
+++ app/src/components/PullRequest.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 
 import {colorLum} from '../util/helpers';
 
-function PullRequest(props) {
-    return (
-        <div className={props.item.class}>
-            <div className='item double-item'>
-                <div className='item_id'>{props.item.id}</div>
-                <div className='item_repo'><span className="repo_root">{props.item.reporoot} / </span>
-                    <br/> {props.item.repo}</div>
-            </div>
-            <div className='item item_title'>{props.item.title}</div>
-            <div className='item item_assignee'>{props.item.assignee}</div>
-        </div>
-    );
+function PullRequest(props){
+  return (
+      <div className={props.item.class}>
+          <div className='item double-item'>
+            <div className='item_id'><small>#</small>{props.item.number}</div>
+            <div className='item_repo'><span className="repo_root">{props.item.repository.owner.login}</span> / {props.item.repository.name}</div>
+          </div>
+          <div className='item item_title'>{props.item.pull_request.title}</div>
+          <div className='item item_assignee'>{props.item.pull_request.assignee || 'Unassigned'}</div>
+      </div>
+  );
 }
 
 export default PullRequest;
