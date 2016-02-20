b7cca5b8d5e6c8c5d7fcb546b93d7a79ee0fa0c1
Ramon Gebben <ramon@ra-ge.net>
2016-02-20 17:33:27 +0100

    fixed css bug

--- app/src/components/PullRequest.jsx
+++ app/src/components/PullRequest.jsx
@@ -4,7 +4,7 @@ import {colorLum} from '../util/helpers';
 
 function PullRequest(props){
   return (
-      <div className={props.item.class}>
+      <div className='pull_request'>
           <div className='item double-item'>
             <div className='item_id'><small>#</small>{props.item.number}</div>
             <div className='item_repo'><span className="repo_root">{props.item.repository.owner.login}</span> / {props.item.repository.name}</div>
