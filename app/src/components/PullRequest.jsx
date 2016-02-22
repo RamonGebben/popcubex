76d4c12a7a1d9c233389c1c54b86d87f6d8baf6f
Leonard eshuis <leonardeshuis@Fegefeuer.local>
2016-02-22 19:33:48 +0100

    fix

--- app/src/components/PullRequest.jsx
+++ app/src/components/PullRequest.jsx
@@ -7,10 +7,10 @@ function PullRequest(props){
       <div className='pull_request'>
           <div className='item double-item'>
             <div className='item_id'><small>#</small>{props.item.number}</div>
-            <div className='item_repo'><span className="repo_root">{props.item.repository.owner.login}</span> / {props.item.repository.name}</div>
+            <div className='item_repo'><span className="repo_root">{props.item.repository.owner.login}</span> / <br/>{props.item.repository.name}</div>
           </div>
           <div className='item item_title'>{props.item.pull_request.title}</div>
-          <div className='item item_assignee'>{props.item.pull_request.assignee || 'Unassigned'}</div>
+          <div className='item item_assignee'>{props.item.pull_request.assignee || 'none'}</div>
       </div>
   );
 }
